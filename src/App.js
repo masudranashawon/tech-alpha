17dfe6b8a81212e1545738a4a36f94fe745e2004
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-01-01 20:04:40 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Footer from "./components/Footer";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className='app min-h-screen bg-gray-50 text-lg text-gray-700'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+const Navbar = () => {
+  return (
+    <div className='navbar-bg bg-violet-500 text-violet-50 h-20 flex justify-center items-center'>
+      <div className='navbar container mx-auto flex items-center justify-between'>
+        <div className='left'>
+          <span className='logo text-xl font-semibold'>
+            tech <span className='text-orange-500'>Alfa</span>
+          </span>
+        </div>
+        <div className='right flex items-center gap-5'>
+          <Link to='/' className='nav-link'>
+            Home
+          </Link>
+          <Link to='/products' className='nav-link'>
+            Products
+          </Link>
+          <Link to='/cart'>
+            <span className='cart-icon relative'>
+              <BsCart3 />
+              <span className='cart-counter absolute -top-3 -right-3 text-xs bg-orange-600 font-medium rounded-full w-5 h-5 flex justify-center items-center z-[1]'>
+                10
+              </span>
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
